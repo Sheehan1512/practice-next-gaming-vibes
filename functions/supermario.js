20ce59687f937d1edea06c08a38f493cb5e94214
Sheehan1512 <sheehanvy@gmail.com>
2022-07-02 08:35:18 +0600

    done

--- functions/supermario.js
+++ functions/supermario.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
     console.log('functions ran')
     const data = {name: 'mario', age: 35, job: 'plumber'}
     return {
-        statuscode: 200,
+        statusCode: 200,
         body: JSON.stringify(data)
     }
 }
